61dbf5bffcf9651068ee69f48109db6ec5ebf36c
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-07-21 22:57:54 +0530

    navbar

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -4,26 +4,31 @@ import React from "react";
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="flex items-end justify-between p-8">
-      <h1 className="font-bold">ECOMMERCE</h1>
-      <div className="flex justify-between gap-4 text-[12px] font-bold">
-        <p>Categories</p>
-        <p>Sale</p>
-        <p>Clearance</p>
-        <p>New Stock</p>
-        <p>Trending</p>
+    <div>
+      <div className="flex items-end justify-between p-8">
+        <h1 className="font-bold">ECOMMERCE</h1>
+        <div className="flex justify-between gap-4 text-[12px] font-bold">
+          <p>Categories</p>
+          <p>Sale</p>
+          <p>Clearance</p>
+          <p>New Stock</p>
+          <p>Trending</p>
+        </div>
+        <div>
+          <div className="mb-2 flex justify-end gap-4 bg-red-100 text-[10px]">
+            <p>Help</p>
+            <p>Orders & Returns</p>
+            <p>Hi, John</p>
+          </div>
+          <div className="text-md flex justify-end gap-2">
+            <AiOutlineSearch />
+            <AiOutlineShoppingCart />
+          </div>
+        </div>
       </div>
       <div>
-        <div className="mb-2 flex justify-end gap-4 bg-red-100 text-[10px]">
-          <p>Help</p>
-          <p>Orders & Returns</p>
-          <p>Hi, John</p>
-        </div>
-        <div className="text-md flex justify-end gap-2">
-          <AiOutlineSearch />
-          <AiOutlineShoppingCart />
-        </div>
+        <p>Get 10% off on business signup </p>
       </div>
-    </nav>
+    </div>
   );
 };
